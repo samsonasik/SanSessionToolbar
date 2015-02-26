d398b26cdd737154d00259c52755ebda1d42ffce
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-02-27 04:57:46 +0700

    use Module class inside src/

--- Module.php
+++ Module.php
@@ -1,34 +1,7 @@
 <?php
 /**
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This software consists of voluntary contributions made by many individuals
- * and is licensed under the MIT license.
+ * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/SanSessionToolbar' in order to respect PSR-0
  */
-namespace SanSessionToolbar;
-
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-
-/**
- * @author Abdul Malik Ikhsan <samsonasik@gmail.com>
- */
-class Module implements ConfigProviderInterface
-{
-    /**
-     * {@inheritDoc}
-     */
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
+require_once __DIR__.'/src/SanSessionToolbar/Module.php';
