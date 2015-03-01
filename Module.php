ebbeaa724acc06f2f3af206b393fff896e531246
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-03-01 17:26:02 +0700

    remove root Module.php, rely on composer autoloader, that it will be pointed to src/SanSessionToolbar/Module.php

--- Module.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
- * The original Module.php is in 'src/SanSessionToolbar' in order to respect PSR-0
- */
-require_once __DIR__.'/src/SanSessionToolbar/Module.php';
