f53fa243ef9a0d26c96470a8297bd1108fe3837e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-11-06 07:02:30 -0500

    Applied fixes from StyleCI

--- src/Manager/SessionManager.php
+++ src/Manager/SessionManager.php
@@ -16,7 +16,6 @@
  * This software consists of voluntary contributions made by many individuals
  * and is licensed under the MIT license.
  */
-
 namespace SanSessionToolbar\Manager;
 
 use Zend\Session\Container;
