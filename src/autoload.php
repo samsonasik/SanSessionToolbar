9e4e045f81733fd26b7adb1d919477f25b04e3bb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2019-11-14 08:44:44 +0700

    add class_alias legacy ZendDeveloperTools namespace

---
+++ src/autoload.php
@@ -0,0 +1,8 @@
+<?php 
+
+use LaminasDeveloperTools\Collector\AbstractCollector as LaminasDeveloperToolsAbstractCollector;
+use ZendDeveloperTools\Collector\AbstractCollector as ZendDeveloperToolsAbstractCollector;
+
+if (class_exists(LaminasDeveloperToolsAbstractCollector::class)) {
+	class_alias(LaminasDeveloperToolsAbstractCollector::class, ZendDeveloperToolsAbstractCollector::class);
+}
